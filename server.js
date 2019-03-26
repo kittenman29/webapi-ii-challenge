60bbfea0124b8825061461cd17b06d739eed18c2
Will Schulz <will.schulz29@gmail.com>
2019-03-26 17:07:27 -0400

    initial pull request

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// const Hubs = require('./hubs/hubs-model.js');
+// const hubsRouter = require('./hubs/hubs-router.js')
+
+const server = express();
+
+server.use(express.json());
+
+// server.get('/', (req, res) => {
+//   res.send(`
+//     <h2>Lambda Hubs API</h>
+//     <p>Welcome to the Lambda Hubs API</p>
+//   `);
+// });
+
+// server.use('/api/hubs', hubsRouter);
+
+module.exports = server;
