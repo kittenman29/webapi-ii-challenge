326d1de2940899cddd2680d954855eee129d103a
Will Schulz <will.schulz29@gmail.com>
2019-03-26 23:49:32 -0400

    another crud operation

---
+++ data/posts/post-router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const Posts = require('../db.js');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const posts = await Posts.find(req.query);
+        const { title, contents } = req.body;
+        if(!title || !contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        } 
+        Posts.insert({ title, contents })
+        .then (post => {
+            res.status(201).json(posts);
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find(req.query);
+        res.status(200).json(posts);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "The posts information could not be retrieved" })
+    }
+})
+
+// router.get('/:id', async (req,res) => {
+//     try {
+//         const posts = await 
+//     }
+// })
+module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require('express');
 
-// const Hubs = require('./hubs/hubs-model.js');
-// const hubsRouter = require('./hubs/hubs-router.js')
+const postRouter = require('./data/posts/post-router.js')
 
 const server = express();
 
 server.use(express.json());
 
-// server.get('/', (req, res) => {
-//   res.send(`
-//     <h2>Lambda Hubs API</h>
-//     <p>Welcome to the Lambda Hubs API</p>
-//   `);
-// });
+server.use('/api/posts', postRouter);
 
-// server.use('/api/hubs', hubsRouter);
+server.listen(4000, () => {
+    console.log('\n*** Server Running on http://localhost:4000 ***\n');
+  });
 
-module.exports = server;
