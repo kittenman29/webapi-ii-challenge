e0712e3e07aa43bfb8070b12de92af190d9b749c
Will Schulz <will.schulz29@gmail.com>
2019-03-27 00:01:34 -0400

    more crud methods

--- data/posts/post-router.js
+++ data/posts/post-router.js
@@ -47,5 +47,18 @@ router.get('/:id', async (req,res) => {
     }
 })
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const posts = await Posts.remove(req.params.id);
+        if(count>0) {
+            res.status(200).json({posts});
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "The post could not be removed" })
+    }
+})
 
 module.exports = router;
