3d03c5de01868b757bca50e4bd131b97e54a5281
Will Schulz <will.schulz29@gmail.com>
2019-03-26 23:55:37 -0400

    some editing

--- data/posts/post-router.js
+++ data/posts/post-router.js
@@ -36,8 +36,8 @@ router.get('/:id', async (req,res) => {
     try {
         const posts = await Posts.findById(req.params.id);
 
-        if(post) {
-            res.status(200).json(post);
+        if(posts) {
+            res.status(200).json(posts);
         } else {
             res.status(404).json({ message: "The post with the specified ID does not exist." });
         }
