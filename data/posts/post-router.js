d33b21fa3a325a667d1d5ab50c14f706c06a90db
Will Schulz <will.schulz29@gmail.com>
2019-03-26 23:52:22 -0400

    another crud method done

--- data/posts/post-router.js
+++ data/posts/post-router.js
@@ -32,9 +32,19 @@ router.get('/', async (req, res) => {
     }
 })
 
-// router.get('/:id', async (req,res) => {
-//     try {
-//         const posts = await 
-//     }
-// })
+router.get('/:id', async (req,res) => {
+    try {
+        const posts = await Posts.findById(req.params.id);
+
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "The post information could not be retrieved." })
+    }
+})
+
 module.exports = router;
